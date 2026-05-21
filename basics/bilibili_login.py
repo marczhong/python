5f2940d2841dc66a075fcc47a688c3eeb81d5754
marczhong <114092978+marczhong@users.noreply.github.com>
2026-05-21 01:11:28 +0100

    Delete basics/bilibili_login.py

--- basics/bilibili_login.py
+++
@@ -1,9 +0,0 @@
-account = int(input("Enter your bilibili account"))
-password = int(input("Enter your bilibili password"))
-
-if account == 12345 and password == 88888:
-   print("Correct!")
-
-else:
-   print("Wrong!")
-
