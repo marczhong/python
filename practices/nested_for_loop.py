82cd8d828f6516d0922f83102aa7855db4cc35e2
marczhong <114092978+marczhong@users.noreply.github.com>
2026-05-23 01:54:09 +0100

    Add files via upload

---
+++ practices/nested_for_loop.py
@@ -0,0 +1,15 @@
+# Length
+
+l = int(input("Lenth:"))
+
+# width
+
+w = int(input("Width"))
+
+
+# print 
+for x in range(w): # Lenth
+   for i in range(l): # Width
+      print("*", end ="  ")
+   print()
+
